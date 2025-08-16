908f87bf72ee810575a74987bc3f467fc112b22a
PrzemyslawSwiderski <przemekzgierz7@gmail.com>
2025-08-16 21:18:50 +0200

    chore: Samples version update

--- examples/sample-terraform-project/build.gradle.kts
+++ examples/sample-terraform-project/build.gradle.kts
@@ -1,7 +1,7 @@
 import com.pswidersk.gradle.terraform.TerraformTask
 
 plugins {
-    id("com.pswidersk.terraform-plugin") version "1.1.1"
+    id("com.pswidersk.terraform-plugin") version "1.2.0"
 }
 
 terraformPlugin {
