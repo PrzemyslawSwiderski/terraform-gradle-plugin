7ea99fa37fdcb6fac0a65ea564f26b6dff1a7892
Przemyslaw Swiderski <przemyslaw.swiderski7@gmail.com>
2026-05-01 17:10:12 +0200

    test fix

--- src/test/kotlin/com/pswidersk/gradle/terraform/GradleSupportTest.kt
+++ src/test/kotlin/com/pswidersk/gradle/terraform/GradleSupportTest.kt
@@ -13,7 +13,7 @@ class GradleSupportTest {
     @TempDir
     lateinit var tempDir: File
 
-    @ValueSource(strings = ["8.10.1", "7.5"])
+    @ValueSource(strings = ["8.4", "7.5"])
     @ParameterizedTest
     fun `test if plugin is working for older gradle versions`(gradleVersion: String) {
         // given
